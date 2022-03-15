c9b6921d8aa3ab744fbdd66c30829df283e73efd
Amdjad Anrifou <amdjad.anrifou@etu.iut-tlse3.fr>
2022-03-15 15:50:20 +0100

    Peaufinage mineure

--- chiffrementMessage.c
+++ chiffrementMessage.c
@@ -1,3 +1,23 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+void verifierAlphanumerique(char texte){
+
+}
+
+void convertirAccents(char texte){
+
+}
+
+void chiffrer(char texte){
+
+}
+
+void dechiffrer(char texte){
+
+}
+
+void affichage(char texte){
+
+}
---
+++ chiffrementMessage.h
@@ -0,0 +1,37 @@
+// FICHIER C CONTENANT LES ENTETES DES FONCTIONS DE CHIFFREMENT
+
+/**
+ * @brief Vérifie si le message à chiffrer ou à déchiffrer 
+ * ne comporte que des caractères alphanumériques
+ * @param texte Message à analyser
+ */
+void verifierAlphanumerique(char texte);
+
+/**
+ * @brief Convertit les accents en chaine alphanumériques
+ * @param texte Message contenant des caractères alphanumériques
+ */
+void convertirAccents(char texte);
+
+/**
+ * @brief Retourne une chaine de caractères chiffré
+ * 
+ * @param texte Message à chiffrer
+ * @return char 
+ */
+char chiffrer(char texte);
+
+/**
+ * @brief Retourne une chaine de caractères déchiffré 
+ * 
+ * @param texte Message à déchiffrer
+ * @return char 
+ */
+char dechiffrer(char texte);
+
+/**
+ * @brief Interface
+ * 
+ * @param texte Message à chiffrer ou à déchiffrer
+ */
+void affichage(char texte);
